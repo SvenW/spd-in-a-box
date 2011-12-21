0467f13dad9a0e0ff6946cdec7d2445089ea97d2
Sven Wagener <sven.wagener@rheinschmiede.de>
2011-12-21 22:08:56 +0100

    Fixed Div Bug in page.php

---
+++ slideshow.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<!-- Content //-->
+<div id="content">
+	<div id="photo-rotator">
+	<?php 
+	$slide_id=1; 
+	$featuredPosts = new WP_Query();
+	$featuredPosts->query("showposts=3&category_name=Leitartikel");
+	while ($featuredPosts->have_posts()) : $featuredPosts->the_post();
+	?>     
+		<div id="slide-<?php echo $slide_id; $slide_id++;?>"> 
+			<a href="<?php the_permalink() ?>" class="post-image">
+			<?php the_post_thumbnail( 'rotator-post-image' ); ?>
+			<span class="title"><h2><?php the_title(); ?></h2></span>
+			<span class="text"><p><?php the_excerpt(); ?><br /><span class="more">Weiter zum Artikel</span></p></span>
+		</a>
+	  </div>
+	  <?php endwhile; ?><!--/close loop-->
+	
+		<ul id="slide-nav">
+			<?php $nav_id=1; ?>
+			<?php while ($featuredPosts->have_posts()) : $featuredPosts->the_post(); ?>
+			<li>
+			<a href="#slide-<?php echo $nav_id; ?>">
+				<?$nav_id++;?>
+			</a>
+			</li>
+		 	<?php endwhile; ?><!--/close loop-->
+		</ul>
+	</div> 	
+</div><!-- End of content //-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
